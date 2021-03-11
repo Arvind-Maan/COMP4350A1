b99425f7d61ab4d27ff7288f1c2659bfffcb2003
Arvind Maan <arvindsmaan@gmail.com>
2021-03-11 16:59:05 -0600

    Fix q/a thread

--- src/components/postCard.js
+++ src/components/postCard.js
@@ -9,6 +9,9 @@ const useStyles = makeStyles({
     provider: {
         fontSize: 14,
     },
+    thread: {
+        'margin-left': '50px',
+    },
 });
 const decodeHTML = html => {
     var txt = document.createElement('textarea');
@@ -19,6 +22,8 @@ const PostCard = props => {
     const classes = useStyles();
     const [collapse, setCollapse] = useState(true);
     const [thread, setThread] = useState([]);
+    const [qbody, setQbody] = useState('');
+
     const doCollapse = () => {
         setCollapse(!collapse);
     };
@@ -27,13 +32,21 @@ const PostCard = props => {
         const fetchQuestionInfo = async () => {
             if (!collapse) {
                 const res = await axios.get(
-                    `https://api.stackexchange.com/2.2/questions/${props.question_id}/comments?order=desc&sort=votes&site=stackoverflow&filter=!b6AubjWaofWWu9`
+                    `https://api.stackexchange.com/2.2/questions/${props.question_id}?order=desc&sort=votes&site=stackoverflow&filter=!)rTkraPYPWw39)()ir25`
                 );
                 console.log(res);
-                res.data.items.sort((a, b) =>
+                let thread = [];
+                const question = res.data.items[0];
+
+                if (question.hasOwnProperty('answers'))
+                    thread = thread.concat(question.answers);
+                if (question.hasOwnProperty('comments'))
+                    thread = thread.concat(question.comments);
+                thread.sort((a, b) =>
                     a.creation_date <= b.creation_date ? 1 : -1
                 );
-                setThread(res.data.items);
+                setQbody(decodeHTML(question.body_markdown));
+                setThread(thread);
             }
         };
         fetchQuestionInfo();
@@ -46,30 +59,74 @@ const PostCard = props => {
                     {new Date(props.creation_date * 1000).toLocaleString()}
                 </Typography>
                 <Typography variant="h5">{decodeHTML(props.title)}</Typography>
+                <Typography>{!collapse ? qbody : null}</Typography>
                 <Typography
                     className={
                         classes.date
                     }>{`votes: ${props.votes}`}</Typography>
             </CardContent>
-            <Card variant="outlined">
+
+            <Card>
                 {!collapse
                     ? thread.map((comment, i) => {
+                          let subcomments = null;
+                          if (comment.hasOwnProperty('comments')) {
+                              subcomments = comment.comments;
+                              subcomments.sort((a, b) =>
+                                  a.creation_date <= b.creation_date ? 1 : -1
+                              );
+                          }
+
                           return (
-                              <CardContent key={i}>
-                                  <Typography color="textSecondary">
-                                      {new Date(
-                                          comment.creation_date * 1000
-                                      ).toLocaleString()}
-                                  </Typography>
-                                  <Typography variant="subtitle1">
-                                      {decodeHTML(comment.body)}
-                                  </Typography>
-                                  <Typography
-                                      variant="caption"
-                                      className={
-                                          classes.date
-                                      }>{`votes: ${props.votes}`}</Typography>
-                              </CardContent>
+                              <Card
+                                  variant="outlined"
+                                  className={classes.thread}
+                                  key={i}>
+                                  <CardContent>
+                                      <Typography color="textSecondary">
+                                          {new Date(
+                                              comment.creation_date * 1000
+                                          ).toLocaleString()}
+                                      </Typography>
+                                      <Typography variant="subtitle1">
+                                          {decodeHTML(comment.body_markdown)}
+                                      </Typography>
+                                      <Typography
+                                          variant="caption"
+                                          className={
+                                              classes.date
+                                          }>{`votes: ${props.votes}`}</Typography>
+                                  </CardContent>
+                                  {subcomments
+                                      ? subcomments.map((subcomment, i) => {
+                                            return (
+                                                <Card
+                                                    variant="outlined"
+                                                    className={classes.thread}
+                                                    key={i}>
+                                                    <CardContent>
+                                                        <Typography color="textSecondary">
+                                                            {new Date(
+                                                                subcomment.creation_date *
+                                                                    1000
+                                                            ).toLocaleString()}
+                                                        </Typography>
+                                                        <Typography variant="subtitle1">
+                                                            {decodeHTML(
+                                                                subcomment.body_markdown
+                                                            )}
+                                                        </Typography>
+                                                        <Typography
+                                                            variant="caption"
+                                                            className={
+                                                                subcomment.date
+                                                            }>{`votes: ${subcomment.votes}`}</Typography>
+                                                    </CardContent>
+                                                </Card>
+                                            );
+                                        })
+                                      : null}
+                              </Card>
                           );
                       })
                     : null}
